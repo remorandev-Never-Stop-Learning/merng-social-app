e0c016057424ba1fe97f72f6c522ee06dce32a66
remorandev <remorandev@gmail.com>
2020-10-18 18:04:00 +0200

    Setting up mongodb and connecting with apollo server.  - the apollo server now admit getPosts query

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const Post = require("./models/Post");
 const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
+    type Post {
+      id: ID!,
+      body: String!,
+      createdAt: String!,
+      username: String!
+    }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 const resolvers = {
   Query: {
-    sayHi: () => "Hello Word!!!",
+    async getPosts() {
+        try{
+          const post = await Post.find();
+          return post;
+        }catch(err){
+          throw new Error(err)
+        }
+    }
   },
 };
 
@@ -23,7 +37,7 @@ const server = new ApolloServer({
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-      console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
     return server.listen({ port: 5000 });
   })
   .then((res) => {
