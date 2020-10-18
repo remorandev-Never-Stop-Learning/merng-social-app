3d2b8ef5c2c4bdf80aac9f1338ca1c4a29e83291
remorandev <remorandev@gmail.com>
2020-10-18 17:21:30 +0200

    Connecting MongoDB

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const { ApolloServer } = require('apollo-server')
-const gql = require('graphql-tag')
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
-    type Query {
-        sayHi:  String!
-    }
-`
+  type Query {
+    sayHi: String!
+  }
+`;
 const resolvers = {
-    Query: {
-        sayHi: () => 'Hello Word!!!'
-    }
-}
+  Query: {
+    sayHi: () => "Hello Word!!!",
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
-server.listen({ port: 5000 })
-        .then(res => {
-            console.log(`Server running at ${res.url}`)
-        })
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+      console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
